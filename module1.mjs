dd90cbcd453223c8c34bc131255ec82528417c52
Milan Hirpara <hirparamilan@gmail.com>
2026-01-07 20:20:51 +0530

    Java script concept .mjs, add bootstap and use navbar

---
+++ module1.mjs
@@ -0,0 +1,8 @@
+import dz, {a, c, d} from './module2.mjs'
+
+// Just a java script concept
+
+console.log(dz);
+console.log(c);
+console.log(d);
+console.log(a);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a="Milan";
+const b="Rahul";
+const c="Sumit";
+const d="Sapna";
+
+export default b;
+export {a};
+export {c};
+export {d};
